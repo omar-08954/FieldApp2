f7319d98420d01a287420731d9185c91910da0ed
Ahmed Shahin <satec3030@gmail.com>
2026-07-13 10:25:58 +0300

    Update FieldApp

--- scripts/setup_database.py
+++ scripts/setup_database.py
@@ -1,5 +1,10 @@
+import os
+import sys
+
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from database.database import create_tables
 
 create_tables()
 
-print("Database initialized successfully.")
+print("✅ Database initialized successfully.")
